695c3da7ce8c1db9259cfc8128cc0007f2f11de1
Vimukthi Kavinda <69883449+vimukthi-Kavinda@users.noreply.github.com>
2025-06-29 22:38:15 +0530

    Update SecurityConfig.java - .authenticated() added for all requests

--- src/main/java/com/example/security1/config/SecurityConfig.java
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 		
 		
 		return http.csrf(csrf->csrf.disable())//csrf disable   -> each line a filter in filter service?
-				.authorizeHttpRequests(req->req.anyRequest())// auth all incoming request
+				.authorizeHttpRequests(req->req.anyRequest().authenticated())// auth all incoming request
 				.httpBasic(Customizer.withDefaults()) // for postman like requests
 				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))// new session id per each req
 				.build();
