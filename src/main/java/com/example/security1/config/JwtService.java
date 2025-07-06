5effafe6c64023e3f4cfca4fb69c867ea81c169d
vimukthik <vimukthi2017kavinda@gmail.com>
2025-07-06 13:45:57 +0530

    jwt integration - secret key for jwt token

--- src/main/java/com/example/security1/config/JwtService.java
+++ src/main/java/com/example/security1/config/JwtService.java
@@ -1,16 +1,23 @@
 package com.example.security1.config;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.crypto.KeyGenerator;
+
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
+	String secret = "12345678901234567890123456789012";
+	
 	public String generateToken(String userName) {
 		
 		Map<String ,Object> claims = new HashMap<String, Object>();
@@ -22,8 +29,13 @@ public class JwtService {
 				.issuedAt(new Date(System.currentTimeMillis()))
 				.expiration(new Date(System.currentTimeMillis()+60*60))
 				.and()
-				//.signWith(null)
+				.signWith(getKey())
 				.compact();
+	}
+
+
+	private Key getKey() {
+		return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 	}
 
 }
